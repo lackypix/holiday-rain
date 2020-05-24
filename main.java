9f1eb1422dc26e321f7f66e0c11c1d5cbbf9e9f9
Amir <53087797+baza04@users.noreply.github.com>
2020-05-24 14:30:31 +0600

    изменил переменную difficult
    Поменял название с difficult на hard

--- main.java
+++ main.java
@@ -4,11 +4,11 @@ public class main {
 	public static void main(String[] args) {				
 		
 		String choise = JOptionPane.showInputDialog(null, "Choose the difficulty of the game from 0 to 7", "Game difficulty", 1);
-		int difficult = choise.charAt(0)-'0';					// Transfer choice value to integer type variable take char and subtract char 'zero'
+		int hard = choise.charAt(0)-'0';					// Transfer choice value to integer type variable take char and subtract char 'zero'
 		
-		if ((difficult >= 1) && (difficult <= 7)) {
-			window nw = new window(difficult);					
+		if ((hard >= 1) && (hard <= 7)) {
+			window nw = new window(hard);					
 		}
-				
+			
 	}
 }
